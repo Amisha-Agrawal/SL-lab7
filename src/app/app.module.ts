e0350137913af6fee25a1f234a4acba43c407753
Amisha Agrawal <Amisha Agrawal>
2020-10-18 22:42:09 +0530

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodoHeaderComponent } from './todo/todo-header/todo-header.component';
@@ -10,6 +12,7 @@ import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { TododetailsComponent } from './tododetails/tododetails.component';
+import { TodocreateComponent } from './todocreate/todocreate.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +23,13 @@ import { TododetailsComponent } from './tododetails/tododetails.component';
     TodoListComponent,
     TodoFooterComponent,
     TodolistComponent,
-    TododetailsComponent
+    TododetailsComponent,
+    TodocreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
