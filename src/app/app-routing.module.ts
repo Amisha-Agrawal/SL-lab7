7ae82b4dcbcb30ec7327cda98026eff3a37270a1
Amisha Agrawal <Amisha Agrawal>
2020-11-04 13:43:03 +0530

    message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TododetailsComponent } from './tododetails/tododetails.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', component: TodolistComponent },
+   { path: 'ed' , component: TododetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
